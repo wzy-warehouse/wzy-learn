57049352c29dba107393bff6ae542eb58dafc70c
wzy-warehouse <18135009705@163.com>
2025-07-09 22:28:33 +0800

    添加静态资源

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -1,18 +1,12 @@
-import './assets/main.css'
+import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { getCategory } from './apis/testAPI'
 
 const app = createApp(App)
-
-// 测试接口
-getCategory().then(res => {
-  console.log(res)
-})
 
 app.use(createPinia())
 app.use(router)
